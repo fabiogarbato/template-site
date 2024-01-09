31dc25e0293730fceb626038ce0255df017b7e8b
Fabio <fabio.garbato@mps.com>
2024-01-09 15:45:56 -0300

    commit de features

--- src/App.js
+++ src/App.js
@@ -7,16 +7,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <Container fluid style={{ backgroundColor: 'grey', minHeight: '100vh' }}>
-        <Navbar bg="light" expand="lg">
+    <Container fluid style={{ backgroundColor: 'black', minHeight: '100vh' }}>
+        <Navbar bg="black" expand="lg">
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ms-auto">
-                    <Nav.Link href="#home" style={{ margin: '0 20px', fontSize: '32px', color: 'black' }}>  Home  </Nav.Link>
-                    <Nav.Link href="#link" style={{ margin: '0 20px', fontSize: '32px', color: 'black' }}>Pagina 1</Nav.Link>
-                    <Nav.Link href="#link" style={{ margin: '0 20px', fontSize: '32px', color: 'black' }}>Pagina 2</Nav.Link>
-                    <Nav.Link href="#link" style={{ margin: '0 20px', fontSize: '32px', color: 'black' }}>Pagina 3</Nav.Link>
-                    <Nav.Link href="#link" style={{ margin: '0 20px', fontSize: '32px', color: 'black' }}>Pagina 4</Nav.Link>
+                    <Nav.Link href="#home" className="nav-link-hover" style={{ margin: '0 20px', fontSize: '32px', color: 'white' }}>  Home  </Nav.Link>
+                    <Nav.Link href="#link" className="nav-link-hover" style={{ margin: '0 20px', fontSize: '32px', color: 'white' }}>Pagina 1</Nav.Link>
+                    <Nav.Link href="#link" className="nav-link-hover" style={{ margin: '0 20px', fontSize: '32px', color: 'white' }}>Pagina 2</Nav.Link>
+                    <Nav.Link href="#link" className="nav-link-hover" style={{ margin: '0 20px', fontSize: '32px', color: 'white' }}>Pagina 3</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
