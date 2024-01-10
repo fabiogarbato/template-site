defae2d30940b16c82e42a232d366cc1601433b5
Fabio <fabio.garbato@mps.com>
2024-01-10 14:14:13 -0300

    Adição do carrosel de imagens

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './index.css';
 import React from 'react';
-import Container from 'react-bootstrap/Container';
+import {Container, Row, Col, Carousel}  from 'react-bootstrap';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <Container fluid style={{ backgroundColor: 'black', minHeight: '100vh' }}>
-        <Navbar bg="black" expand="lg">
+    <Container fluid style={{ backgroundColor: 'blue', minHeight: '100vh' }}>
+        <Navbar bg="blue" expand="lg">
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ms-auto">
@@ -19,9 +19,19 @@ function App() {
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
-        <Container fluid className="py-5 background-section">
-            {/* Conteúdo da Seção */}
-        </Container> 
+        <Container fluid className="background-section">
+            <Carousel>
+                <Carousel.Item>
+                    <div style={{ backgroundColor: 'red', width: '100%', height: '50vh' }}></div>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <div style={{ backgroundColor: 'green', width: '100%', height: '50vh' }}></div>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <div style={{ backgroundColor: 'black', width: '100%', height: '50vh' }}></div>
+                </Carousel.Item>
+            </Carousel>
+        </Container>
     </Container>
   );
 }
