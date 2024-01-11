4d72ee5345a7b59d833397bf60beb4fa69726af5
Fabio <fabio.garbato@mps.com>
2024-01-11 14:11:20 -0300

    Adição de botao whatsapp no site

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import {Container, Carousel, Row, Col}  from 'react-bootstrap';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { FaWhatsapp } from 'react-icons/fa';
+
+const WhatsAppButton = ({ phoneNumber }) => {
+    const whatsappLink = `https://wa.me/55${phoneNumber}?text=Adorei%20seu%20artigo`;
+    return (
+      <a href={whatsappLink} className="whatsapp-float" target="_blank" rel="noopener noreferrer">
+        <FaWhatsapp />
+      </a>
+    );
+  };
 
 function App() {
   return (
@@ -35,18 +45,19 @@ function App() {
         <Container className='background-section2'>
             <Row className='h-100 d-flex justify-content-center align-items-center' style={{ backgroundColor: 'pink'}}>
                 <Col className='d-flex justify-content-center align-items-center'>
-                    <span>Texto centralizado</span>
+                    <span className='text-first'>Texto principal</span>
                 </Col>
             </Row>
             <Row>
                 <Col xs={12} lg={6} className='d-flex justify-content-center align-items-center' style={{ backgroundColor: 'green', height: '70vh'}}>
-                    <span>Coluna da esquerda</span>
+                    <span className='text-before'>Lorem ipsum odio faucibus elementum fames neque class pellentesque taciti, ut magna fermentum quis leo laoreet fusce potenti, augue ultrices etiam augue sed felis bibendum suspendisse. eu aliquam augue velit eu sollicitudin odio justo, blandit duis sit purus ultricies augue venenatis, neque purus habitant quis mi himenaeos. dolor laoreet eu congue nullam erat himenaeos dictum, inceptos varius nec vulputate ante donec vitae augue, auctor cursus molestie cursus euismod facilisis. commodo scelerisque nisi sed fermentum lacinia amet quisque maecenas accumsan, imperdiet varius ullamcorper orci felis quisque sem habitasse, hac ac nec pharetra integer vestibulum quis porttitor. </span>
                 </Col>
                 <Col xs={12} lg={6} className='d-flex justify-content-center align-items-center' style={{ backgroundColor: 'orange', height: '70vh'}}>
-                    <span>Coluna da direita</span>
+                    <span className='text-before'>Lorem ipsum odio faucibus elementum fames neque class pellentesque taciti, ut magna fermentum quis leo laoreet fusce potenti, augue ultrices etiam augue sed felis bibendum suspendisse. eu aliquam augue velit eu sollicitudin odio justo, blandit duis sit purus ultricies augue venenatis, neque purus habitant quis mi himenaeos. dolor laoreet eu congue nullam erat himenaeos dictum, inceptos varius nec vulputate ante donec vitae augue, auctor cursus molestie cursus euismod facilisis. commodo scelerisque nisi sed fermentum lacinia amet quisque maecenas accumsan, imperdiet varius ullamcorper orci felis quisque sem habitasse, hac ac nec pharetra integer vestibulum quis porttitor. </span>
                 </Col>
             </Row>
         </Container>
+        <WhatsAppButton phoneNumber="41987372059" />
     </Container>
   );
 }
